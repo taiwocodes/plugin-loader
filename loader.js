72e7c812f5473c0cb8cd55265111b845e9b9fff8
taiwo <70769445+taiwocodes@users.noreply.github.com>
2024-02-28 16:04:00 +0100

    add cleave documentation to loader

--- loader.js
+++ loader.js
@@ -5,11 +5,20 @@ function loadCSS(href) {
   document.head.appendChild(link);
 }
 
-function loadJS(src) {
+function loadJS(src, callback) {
   const script = document.createElement('script');
   script.src = src;
+  script.onload = () => {
+    if (callback) {
+      callback();
+    }
+  };
   document.body.appendChild(script);
 }
+
+loadJS('https://cdnjs.cloudflare.com/ajax/libs/cleave.js/1.6.0/cleave.min.js', function() {
+  console.log('Cleave.js loaded');
+});
 
 loadCSS('https://staging.zeepjet.com/zeepjet-main.css');
 loadJS('https://staging.zeepjet.com/zeepjet-main.js');
